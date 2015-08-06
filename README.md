678af25873e6d88d24b10526744988fa52cae39d
Supasate Choochaisri <supasate.c@gmail.com>
2015-08-07 00:42:04 +0700

    Initial repo

---
+++ README.md
@@ -0,0 +1,21 @@
+# RDF Dublin Core to DSpace Dublin Core Converter
+A simple commmand line tool that converts RDF Dublin Core to DSpace Dublin Core Simple Archive Format.
+
+---
+
+## Usage
+```
+python rdfdc2dspace.py <filename>
+```
+*\<filename\>* is a RDF Dublin Core file.
+The program will generate the Dspace Dublin Coer Simple Archive Format in the following structure.
+```
+archive_directory
+  |-- item_00001
+  |     |-- dublin_core.xml
+  |     |-- contents
+  |-- item_00002
+  |     |-- dublin_core.xml
+  |     |-- contents
+  |...
+```
---
+++ rdfdc2dspace.py
@@ -0,0 +1,45 @@
+#coding=utf8
+
+import sys
+import re
+import os
+
+ARCHIVE_DIR_NAME = 'archive_directory'
+DUBLIN_CORE_FILE_NAME = 'dublin_core.xml'
+CONTENTS_FILE_NAME = 'contents'
+
+if len(sys.argv) < 2:
+    print('Please specify a file to be converted as the first argument.')
+    exit()
+
+filename = sys.argv[1]
+
+with open(filename, "r") as f:
+    content = f.read()
+    # Convert RDF Dublin Core to DSpace Dublin Core Simple Archive Format
+    content = re.sub(r'\s\s<(\/?)rdf:Description', r'<\1dublin_core', content)
+    content = re.sub(r'(\.|\s|\,|\/)*</dc:.+>', r'</dcvalue>', content)
+    content = re.sub(r'\s\s<dc:(\w+)>', r'<dcvalue element="\1">', content)
+    content = re.sub(r'<(\/)?rdf:RDF.*>', '', content)
+
+    # Convert some elements to supported DSpace elements
+    content = re.sub(r'element="creator"', r'element="contributor" qualifier="author"', content)
+    content = re.sub(r'element="date"', r'element="date" qualifier="issued"', content)
+    content = re.sub(r'element="identifier"', r'element="relation" qualifier="uri"', content)
+    content = re.sub(r'element="format"', r'element="format" qualifier="mimetype"', content)
+
+    # Create Simple Archive Structure to be imported to DSpace
+    os.mkdir(ARCHIVE_DIR_NAME)
+
+    # Extract each item to create its own directory structure
+    for idx, item in enumerate(re.findall(r'(<dublin_core>.*?</dublin_core>)', content, flags=re.DOTALL)):
+        item_dir = ARCHIVE_DIR_NAME + '/item_' + str(idx + 1).zfill(5)
+        os.mkdir(item_dir)
+        with open(item_dir + '/'+ DUBLIN_CORE_FILE_NAME, 'w') as xml_file:
+            xml_file.write('<?xml version="1.0" encoding="utf-8"?>\n')
+            xml_file.write(item + '\n')
+
+        # Create empty contents file that is mandatory for importing to DSpace
+        # (Actually, this file is used to specify bistream file names)
+        open(item_dir + '/' + CONTENTS_FILE_NAME, 'w').close()
+
